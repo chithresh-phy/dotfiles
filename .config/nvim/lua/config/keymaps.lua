c821dce82b3559cb3f86a13b4c9ee5813a057611
chithresh-phy <176747366+chithresh-phy@users.noreply.github.com>
2025-08-11 21:59:12 +0530

    rewritten config from scratch

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,28 @@
+-- Better Defaults --
+
+-- For conciseness
+local opts = { noremap = true, silent = true }
+
+-- delete single character without copying into register
+vim.keymap.set('n', 'x', '"_x', opts)
+
+-- Vertical scroll and center
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+
+-- Find and center
+vim.keymap.set('n', 'n', 'nzzzv', opts)
+vim.keymap.set('n', 'N', 'Nzzzv', opts)
+
+-- Stay in indent mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set('v', 'p', '"_dP', opts)
+
+
+-- Custom Keybinds --
+
+-- Oil.nvim
+vim.keymap.set('n', '-', '<cmd> Oil --float <CR>', {desc="Open Parent Directory in Oil"})
--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -5,5 +5,8 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "zathura"
-  end
+    --quickfix settings
+    vim.g.vimtex_quickfix_open_on_warning = 0 -- don't open quickfix if there are only warnings
+    vim.g.vimtex_quickfix_ignore_filters = {"Underfull","Overfull", "LaTeX Warning: .\\+ float specifier changed to", "Package hyperref Warning: Token not allowed in a PDF string"}
+end
 }
